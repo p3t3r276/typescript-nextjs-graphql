c96a509115aee5a66ccdbf7f2d17896606d6a80f
p3t3r276 <p3t3r276@gmail.com>
2020-03-05 10:12:39 +0700

    formik register form

--- generated/apolloComponents.tsx
+++ generated/apolloComponents.tsx
@@ -1,160 +1,182 @@
-import gql from "graphql-tag";
-import * as ApolloReactCommon from "@apollo/react-common";
-import * as React from "react";
-import * as ApolloReactComponents from "@apollo/react-components";
-import * as ApolloReactHoc from "@apollo/react-hoc";
+import gql from 'graphql-tag';
+import * as ApolloReactCommon from '@apollo/react-common';
+import * as React from 'react';
+import * as ApolloReactComponents from '@apollo/react-components';
+import * as ApolloReactHoc from '@apollo/react-hoc';
 export type Maybe<T> = T | null;
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
-  Upload: any;
+  ID: string,
+  String: string,
+  Boolean: boolean,
+  Int: number,
+  Float: number,
+  Upload: any,
 };
 
 export type ChangePasswordInput = {
-  password: Scalars["String"];
-  token: Scalars["String"];
+  password: Scalars['String'],
+  token: Scalars['String'],
 };
 
 export type Mutation = {
-  __typename?: "Mutation";
-  register: User;
-  changePassword?: Maybe<User>;
-  confirmUser: Scalars["Boolean"];
-  forgotPassword: Scalars["Boolean"];
-  login?: Maybe<User>;
-  logout: Scalars["Boolean"];
-  createUser: User;
-  addProfilePicture: Scalars["Boolean"];
+   __typename?: 'Mutation',
+  register: User,
+  changePassword?: Maybe<User>,
+  confirmUser: Scalars['Boolean'],
+  forgotPassword: Scalars['Boolean'],
+  login?: Maybe<User>,
+  logout: Scalars['Boolean'],
+  createUser: User,
+  addProfilePicture: Scalars['Boolean'],
 };
+
 
 export type MutationRegisterArgs = {
-  data: RegisterInput;
+  data: RegisterInput
 };
+
 
 export type MutationChangePasswordArgs = {
-  data: ChangePasswordInput;
+  data: ChangePasswordInput
 };
+
 
 export type MutationConfirmUserArgs = {
-  token: Scalars["String"];
+  token: Scalars['String']
 };
+
 
 export type MutationForgotPasswordArgs = {
-  email: Scalars["String"];
+  email: Scalars['String']
 };
+
 
 export type MutationLoginArgs = {
-  password: Scalars["String"];
-  email: Scalars["String"];
+  password: Scalars['String'],
+  email: Scalars['String']
 };
+
 
 export type MutationCreateUserArgs = {
-  data: RegisterInput;
+  data: RegisterInput
 };
 
+
 export type MutationAddProfilePictureArgs = {
-  picture: Scalars["Upload"];
+  picture: Scalars['Upload']
 };
 
 export type Query = {
-  __typename?: "Query";
-  Hello: Scalars["String"];
-  me?: Maybe<User>;
+   __typename?: 'Query',
+  Hello: Scalars['String'],
+  me?: Maybe<User>,
 };
 
 export type RegisterInput = {
-  password: Scalars["String"];
-  firstName: Scalars["String"];
-  lastName: Scalars["String"];
-  email: Scalars["String"];
+  password: Scalars['String'],
+  firstName: Scalars['String'],
+  lastName: Scalars['String'],
+  email: Scalars['String'],
 };
 
+
 export type User = {
-  __typename?: "User";
-  id: Scalars["ID"];
-  firstName: Scalars["String"];
-  lastName: Scalars["String"];
-  email: Scalars["String"];
-  name: Scalars["String"];
+   __typename?: 'User',
+  id: Scalars['ID'],
+  firstName: Scalars['String'],
+  lastName: Scalars['String'],
+  email: Scalars['String'],
+  name: Scalars['String'],
 };
 
 export type LoginMutationVariables = {
-  email: Scalars["String"];
-  password: Scalars["String"];
+  email: Scalars['String'],
+  password: Scalars['String']
 };
 
-export type LoginMutation = { __typename?: "Mutation" } & {
-  login: Maybe<
-    { __typename?: "User" } & Pick<
-      User,
-      "id" | "firstName" | "lastName" | "email" | "name"
-    >
-  >;
-};
 
-export const LoginDocument = gql`
-  mutation Login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      id
-      firstName
-      lastName
-      email
-      name
-    }
-  }
-`;
-export type LoginMutationFn = ApolloReactCommon.MutationFunction<
-  LoginMutation,
-  LoginMutationVariables
->;
-export type LoginComponentProps = Omit<
-  ApolloReactComponents.MutationComponentOptions<
-    LoginMutation,
-    LoginMutationVariables
-  >,
-  "mutation"
->;
-
-export const LoginComponent = (props: LoginComponentProps) => (
-  <ApolloReactComponents.Mutation<LoginMutation, LoginMutationVariables>
-    mutation={LoginDocument}
-    {...props}
-  />
+export type LoginMutation = (
+  { __typename?: 'Mutation' }
+  & { login: Maybe<(
+    { __typename?: 'User' }
+    & Pick<User, 'id' | 'firstName' | 'lastName' | 'email' | 'name'>
+  )> }
 );
 
-export type LoginProps<TChildProps = {}> = ApolloReactHoc.MutateProps<
-  LoginMutation,
-  LoginMutationVariables
-> &
-  TChildProps;
-export function withLogin<TProps, TChildProps = {}>(
-  operationOptions?: ApolloReactHoc.OperationOption<
-    TProps,
-    LoginMutation,
-    LoginMutationVariables,
-    LoginProps<TChildProps>
-  >
-) {
-  return ApolloReactHoc.withMutation<
-    TProps,
-    LoginMutation,
-    LoginMutationVariables,
-    LoginProps<TChildProps>
-  >(LoginDocument, {
-    alias: "login",
-    ...operationOptions
-  });
-}
-export type LoginMutationResult = ApolloReactCommon.MutationResult<
-  LoginMutation
->;
-export type LoginMutationOptions = ApolloReactCommon.BaseMutationOptions<
-  LoginMutation,
-  LoginMutationVariables
->;
+export type RegisterMutationVariables = {
+  data: RegisterInput
+};
 
+
+export type RegisterMutation = (
+  { __typename?: 'Mutation' }
+  & { register: (
+    { __typename?: 'User' }
+    & Pick<User, 'firstName' | 'lastName' | 'email' | 'name'>
+  ) }
+);
+
+
+export const LoginDocument = gql`
+    mutation Login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    id
+    firstName
+    lastName
+    email
+    name
+  }
+}
+    `;
+export type LoginMutationFn = ApolloReactCommon.MutationFunction<LoginMutation, LoginMutationVariables>;
+export type LoginComponentProps = Omit<ApolloReactComponents.MutationComponentOptions<LoginMutation, LoginMutationVariables>, 'mutation'>;
+
+    export const LoginComponent = (props: LoginComponentProps) => (
+      <ApolloReactComponents.Mutation<LoginMutation, LoginMutationVariables> mutation={LoginDocument} {...props} />
+    );
+    
+export type LoginProps<TChildProps = {}> = ApolloReactHoc.MutateProps<LoginMutation, LoginMutationVariables> & TChildProps;
+export function withLogin<TProps, TChildProps = {}>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
+  LoginMutation,
+  LoginMutationVariables,
+  LoginProps<TChildProps>>) {
+    return ApolloReactHoc.withMutation<TProps, LoginMutation, LoginMutationVariables, LoginProps<TChildProps>>(LoginDocument, {
+      alias: 'login',
+      ...operationOptions
+    });
+};
+export type LoginMutationResult = ApolloReactCommon.MutationResult<LoginMutation>;
+export type LoginMutationOptions = ApolloReactCommon.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
+export const RegisterDocument = gql`
+    mutation Register($data: RegisterInput!) {
+  register(data: $data) {
+    firstName
+    lastName
+    email
+    name
+  }
+}
+    `;
+export type RegisterMutationFn = ApolloReactCommon.MutationFunction<RegisterMutation, RegisterMutationVariables>;
+export type RegisterComponentProps = Omit<ApolloReactComponents.MutationComponentOptions<RegisterMutation, RegisterMutationVariables>, 'mutation'>;
+
+    export const RegisterComponent = (props: RegisterComponentProps) => (
+      <ApolloReactComponents.Mutation<RegisterMutation, RegisterMutationVariables> mutation={RegisterDocument} {...props} />
+    );
+    
+export type RegisterProps<TChildProps = {}> = ApolloReactHoc.MutateProps<RegisterMutation, RegisterMutationVariables> & TChildProps;
+export function withRegister<TProps, TChildProps = {}>(operationOptions?: ApolloReactHoc.OperationOption<
+  TProps,
+  RegisterMutation,
+  RegisterMutationVariables,
+  RegisterProps<TChildProps>>) {
+    return ApolloReactHoc.withMutation<TProps, RegisterMutation, RegisterMutationVariables, RegisterProps<TChildProps>>(RegisterDocument, {
+      alias: 'register',
+      ...operationOptions
+    });
+};
+export type RegisterMutationResult = ApolloReactCommon.MutationResult<RegisterMutation>;
+export type RegisterMutationOptions = ApolloReactCommon.BaseMutationOptions<RegisterMutation, RegisterMutationVariables>;
---
+++ pages/register.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Layout from "../components/Layout";
+import { Formik, Field } from "formik";
+import { InputField } from "../components/fields/InputField";
+import { RegisterComponent } from "../generated/apolloComponents";
+
+export default () => {
+  return (
+    <Layout title="Register Page">
+      <RegisterComponent>
+        {register => (
+          <Formik
+            onSubmit={async data => {
+              const response = await register({
+                variables: {
+                  data
+                }
+              });
+              console.log(response);
+            }}
+            initialValues={{
+              email: "",
+              firstName: "",
+              lastName: "",
+              password: ""
+            }}
+          >
+            {({ handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <Field
+                  name="firstName"
+                  placeholder="firstName"
+                  component={InputField}
+                />
+                <Field
+                  name="lastName"
+                  placeholder="lastName"
+                  component={InputField}
+                />
+                <Field
+                  name="email"
+                  placeholder="email"
+                  component={InputField}
+                />
+                <Field
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                  component={InputField}
+                />
+                <button type="submit">Submit</button>
+              </form>
+            )}
+          </Formik>
+        )}
+      </RegisterComponent>
+    </Layout>
+  );
+};
